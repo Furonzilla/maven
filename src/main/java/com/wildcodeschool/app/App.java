41c79cc2c382585a9b8f667bd4e93c0a5ff1f678
Ario Ngu <daitenshifuron@gmail.com>
2021-12-20 10:26:12 +0100

    quest done

---
+++ src/main/java/com/wildcodeschool/app/App.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
